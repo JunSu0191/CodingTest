0a223361c45fa7b460dfaabde501eb920ee653e5
tlswnstn <tlswnstn0191@naver.com>
2024-07-22 02:03:39 +0900

    if문 6번째까지

---
+++ BeckJoon/src/BeckJoon_1/BeckJoon_11.java
@@ -0,0 +1,11 @@
+public class BeckJoon_11 {
+
+    public static void main(String[] args) {
+
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__) |");
+
+    }
+}
---
+++ BeckJoon/src/BeckJoon_2/BeckJoon_06.java
@@ -0,0 +1,24 @@
+package BeckJoon_2;
+
+import java.util.Scanner;
+
+public class BeckJoon_06 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int C = sc.nextInt();
+ 
+        int min = 60 * A + B;   // 시 -> 분
+        min += C;
+ 
+        int hour = (min / 60) % 24;
+        int minute = min % 60;
+ 
+        System.out.println(hour + " " + minute);
+
+        sc.close();
+    }
+
+}
